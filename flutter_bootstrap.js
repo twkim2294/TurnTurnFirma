06cf038329e7c3f92f7eafd61b298282be59ee01
twkim2294 <taewoo3717@gmail.com>
2025-04-13 22:56:53 +0900

    0413

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2590907164"
+    serviceWorkerVersion: "2836657837"
   }
 });
